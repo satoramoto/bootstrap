0dcf9ceadb8ce4ac22cf5858461398a53463d4f9
Ryan Gavin <ryan.michael.gavin@gmail.com>
2024-12-16 16:51:50 -0500

    XMRig.sh

---
+++ xmrig.sh
@@ -0,0 +1,24 @@
+# Setup for xmrig
+useradd -Umr xmrig
+
+wget https://github.com/xmrig/xmrig/releases/download/v6.22.2/xmrig-6.22.2-linux-static-x64.tar.gz
+
+tar -xvf xmrig-6.22.2-linux-static-x64.tar.gz
+
+cd xmrig-6.22.2
+
+cp config.json /etc/xmrig.json
+
+cp xmrig /usr/local/bin
+
+mkdir /var/log/xmrig
+
+chown xmrig:xmrig /var/log/xmrig
+
+# create systemd file
+wget https://github.com/satoramoto/boostrap/xmrig.service /etc/systemd/system/xmrig.service
+nano /etc/systemd/system/xmrig.service
+
+systemctl daemon-reload
+systemctl enable xmrig
+systemctl start xmrig
