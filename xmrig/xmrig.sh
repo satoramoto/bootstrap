f88b1ec7b1c85bb11a7bd87db123a14d03a4af17
Ryan Gavin <ryan.michael.gavin@gmail.com>
2024-12-16 16:59:20 -0500

    MVP

--- xmrig/xmrig.sh
+++ xmrig/xmrig.sh
@@ -9,17 +9,17 @@ tar -xvf xmrig-6.22.2-linux-static-x64.tar.gz
 
 cd xmrig-6.22.2
 
-cp config.json /etc/xmrig.json
-
 cp xmrig /usr/local/bin
 
 mkdir /var/log/xmrig
 
 chown xmrig:xmrig /var/log/xmrig
 
+# create config file
+wget https://github.com/satoramoto/bootstrap/raw/refs/heads/main/xmrig/xmrig.json /etc/xmrig.json
+
 # create systemd file
 wget https://github.com/satoramoto/bootstrap/raw/refs/heads/main/xmrig/xmrig.service /etc/systemd/system/xmrig.service
 
 systemctl daemon-reload
 systemctl enable xmrig
-systemctl start xmrig
